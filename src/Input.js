a2e993312f74478f7042cdf703ffc71e29dcd0c5
Kashyap <kashyap.kmbc@gmail.com>
2017-11-03 20:18:15 +0530

    Select when the input has a value and is in focus

--- src/Input.js
+++ src/Input.js
@@ -3,8 +3,39 @@ import './InputPrompt.css';
 
 export default class Input extends Component {
 	componentDidUpdate() {
-		if (this.props.autoFocus === true)
-			this.instance.focus();
+		const instance = this.instance;
+
+		if (this.props.autoFocus === true && !this.isFocussed) {
+			this.isFocussed = true;
+			instance.focus();
+			if (this.props.value || this.props.value === 0) {
+				instance.select();
+			}
+		} else {
+			this.isFocussed = false;
+		}
+	}
+
+	handleKeyDown = event => {
+		const { onKeyDown, id } = this.props;
+		const key = event.key;
+
+		switch (key) {
+			case 'ArrowLeft':
+			case 'ArrowRight':
+				if (this.instance) {
+					this.instance.select();
+				}
+				event.preventDefault();
+				break;
+		}
+
+		if (typeof onKeyDown === 'function') {
+			onKeyDown({
+				id,
+				key
+			});
+		}
 	}
 
 	handleOnChange = event => {
@@ -23,6 +54,18 @@ export default class Input extends Component {
 		}
 	}
 
+	handleOnFocus = event => {
+		const { onFocus, id } = this.props;
+
+		if (typeof onFocus === 'function') {
+			onFocus({ id });
+		}
+	}
+
+	handleOnClick = event => {
+		this.instance.select();
+	}
+
 	setRef = element => this.instance = element;
 
 	render () {
@@ -31,12 +74,18 @@ export default class Input extends Component {
 		return (
 			<input
 				ref={this.setRef}
-				id={id}
+				id={`input-${id}`}
+				name={`input-${id}`}
 				type='text'
 				maxLength={1}
+				size={1}
 				className='input-field'
 				onChange={this.handleOnChange}
+				onKeyDown={this.handleKeyDown}
+				onFocus={this.handleOnFocus}
 				autoFocus={autoFocus}
+				autoComplete="off"
+				onClick={this.handleOnClick}
 			/>
 		)
 	}
